f544bde4a834e6ab2e9cf4a59709e6adb6bd479e
chemicalfiend <teamkagaku101@gmail.com>
2021-11-29 01:01:48 +0530

    Cylinder Program Done

---
+++ Cylinder/shift.jl
@@ -0,0 +1,18 @@
+#=
+
+Program to shift the atoms to an appropriate origin.
+
+
+=#
+
+
+
+fi = open("supercell.xyz", "r")
+
+lines = readlines(fi)
+
+
+
+fo = open("supercell_shifted.xyz", "w")
+
+
